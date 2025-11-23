fc5e9c8c8d528ec4b2c89262e4bfbac95076b08f
VadimIvanov-yaoo <vi2407751@gmail.com>
2025-11-23 21:53:21 +0300

    Add hero section responsiveness and implement tour section

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll(
+  '#tour__link1, #tour__link2, #tour__link3, #tour__link4, #tour__link5'
+)
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    links.forEach((l) => l.classList.remove('tour__list-link-active'))
+    link.classList.add('tour__list-link-active')
+  })
+})
