321b1fc562a7aa88b8022494785037573863aefe
VadimIvanov-yaoo <vi2407751@gmail.com>
2025-12-30 15:56:09 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/YourTour-snp/',
-})
+// import { defineConfig } from 'vite'
+//
+// export default defineConfig({
+//   base: '/YourTour-snp/',
+// })
